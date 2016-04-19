936a2e0ea3d7c35b6a0d8a6834e6e3a80f528b02
Larel Bruton <larel_b@knights.ucf.edu>
2016-04-18 20:07:19 -0400

    Added mailer and view

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,12 @@
+class ContactMailer < ActionMailer::Base
+	default to: 'larelthedev@gmail.com'
+
+	def contact_email(name, email, body)
+		@name = name
+		@email = email
+		@body = body
+	end
+
+	mail(from: email, subject: 'Contact Form Message')
+
+end
